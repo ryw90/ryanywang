1f9a3fef0a46636314dda2fb7f4bfd9200d17721
Ryan Wang <rywang@outlook.com>
2014-01-25 23:04:14 -0600

    changed FREEZER_DESTINATION

--- flask/build_site.py
+++ flask/build_site.py
@@ -6,6 +6,7 @@ from flask_frozen import Freezer
 DEBUG = True
 FLATPAGES_AUTO_RELOAD = DEBUG
 FLATPAGES_EXTENSION = '.md'
+FREEZER_DESTINATION = '../'
 
 app = Flask(__name__)
 app.config.from_object(__name__)
