19155a8d3001577bc625e2ecb456a42efbde3385
Ryan Wang <ryan@stripe.com>
2014-04-27 18:34:36 -0700

    initial stripe integration

--- ryw.py
+++ ryw.py
@@ -1,12 +1,18 @@
-from flask import Flask, redirect, render_template, url_for
+import os
+import stripe
+from config import *
+from flask import Flask, redirect, render_template, url_for, request
 from flask_flatpages import FlatPages
 
+# Configurations
 FLATPAGES_AUTO_RELOAD = True
 FLATPAGES_EXTENSION = '.md'
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 pages = FlatPages(app)
+stripe.api_key = stripe_keys['secret_key']
+
 
 @app.route('/')
 def index():
@@ -23,6 +29,30 @@ def page(path):
     page = pages.get(path, default={'title':path,
                                     'html':'Under Construction...'})
     return render_template('base.html', page=page)
+
+@app.route('/reading_list')
+def reading_list():
+    return render_template('reading_list.html',
+                           page={'title': 'reading_list'},
+                           key=stripe_keys['publishable_key'])
+
+@app.route('/charge', methods=['POST'])
+def charge():
+    amount = 99
+
+    customer = stripe.Customer.create(
+        email = 'customer@example.com',
+        card = request.form['stripeToken']
+    )
+
+    charge = stripe.Charge.create(
+        customer = customer.id,
+        amount = amount,
+        currency = 'usd',
+        description = 'Flask Charge'
+    )
+
+    return render_template('charge.html', amount=amount, page={'title': 'paid!'})
 
 if __name__=='__main__':
     app.run()
